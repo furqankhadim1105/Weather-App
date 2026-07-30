93f6a4c6864dc465b9d47245afbf2e2d2376ad32
Furqan Khadim <furqankhadim1105@gmail.com>
2026-07-30 13:00:05 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('weather-app-cache').then((cache) => {
+    caches.open('weather-app-v2').then((cache) => {  // <-- yahan version change kiya
       return cache.addAll([
         './',
         './index.html',
         './script.js',
         './manifest.json'
       ]);
+    })
+  );
+});
+
+// Purana cache delete karne ke liye yeh extra event add kar dein taaki naya version foran load ho:
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== 'weather-app-v2') {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
